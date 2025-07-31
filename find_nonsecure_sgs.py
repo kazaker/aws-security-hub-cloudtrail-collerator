8d84c98c6901765ae054b8a26387667a8f454407
Artem Marusov <kazaker@mail.ru>
2025-07-31 10:31:02 +0200

    small improvements

--- find_nonsecure_sgs.py
+++ find_nonsecure_sgs.py
@@ -56,3 +56,39 @@ if __name__ == '__main__':
         athena_eventname_string='AuthorizeSecurityGroupIngress')
     merge_findings_to_csv('nonsecure_sgs_and_their_creators.csv', enriched_findings)
 
+### For my own use
+    data_for_dm = {}
+    for finding in enriched_findings:
+        if finding['finding_type'] == 'discovered':
+            if ':' in finding['principal']:
+                creator = finding['principal'].split(':')[-1]
+            else:
+                creator = finding['username']
+            if creator in data_for_dm:
+                data_for_dm[creator].append(finding['sg_id'])
+            else:
+                data_for_dm[creator] = [finding['sg_id']]
+        else:
+            pass
+    print(data_for_dm)
+
+    for creator in data_for_dm.keys():
+        print(f'''Привет {creator}. Меня зовут Артем, я из команды Security Services.
+Сейчас я занимаюсь анализом конфигурации наших AWS аккаунтов, и в процессе нашел несколько Security Groups, который, судя по логам, были созданы тобой.
+Проблема с найденными группами в том, что они настроены не слишком безопасно, отрывая доступ к критичным сетевым портам портам (таким как SSH, RDP, FTP и т.д.) для всего Интернета.
+Такая конфигурация создает угрозу для нашей инфраструктуры, поэтому хотелось бы попробовать их пофиксить.
+Вот список SGs, о которых идет речь:
+''')
+        for sg in data_for_dm[creator]:
+            print(f'- {sg}')
+        print(f'в аккаунте <account_ID>')
+        print(f'''
+Было бы здорово, если бы у тебя получилось посымотреть на них и сделать что-то из следующего:
+- удалить их, если они уже не используются;
+- настроить их так образом, чтобы ingress правила разрешали доступ только с явно заданных IP адресов;
+- посмотреть, подходит ли под твои юзкейсы наши managed prefix lists. Если коротко, эта фича может помочь легко ограничить доступ только с тех адресов, которые принадлежат площадкам JB. Как этим пользоваться, можно прочитать тут - https://youtrack.jetbrains.com/articles/AWS-A-24/AWS-Managed-prefix-lists
+- посмотреть, подходит ли под твои юзкейсы сервис Systems Manager. Он позволяет подключаться к EC2 вообще без необходимости создавать ingress правила в SG. Подробнее можно почитать тут - https://youtrack.jetbrains.com/articles/AWS-A-25/AWS-Systems-Manager-SSM
+''')
+        print('''
+#########################################
+    ''')
